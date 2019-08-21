aacb71d6754c478d11faf87afe7901220fffaf48
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2019-08-21 23:30:42 +0000

    i just completed the website without the functionalities of signing up and responsiveness

--- index.js
+++ index.js
@@ -39,9 +39,72 @@ const mainSlide=() => {
 mainSlide()
 
 
-const bioButton = document.querySelector(".bioButton");
+
+const button1 = document.querySelector(".button1")
+
+const button2 = document.querySelector(".button2")
+const button3 = document.querySelector(".button3")
+const button4 = document.querySelector(".button4")
+const button5 = document.querySelector(".button5")
+
+
+const slide1 = document.querySelector(".secondSectionOfSliderImage1")
+const slide2 = document.querySelector(".secondSectionOfSliderImage2")
+const slide3 = document.querySelector(".secondSectionOfSliderImage3")
+const slide4 = document.querySelector(".secondSectionOfSliderImage4")
+const slide5 = document.querySelector(".secondSectionOfSliderImage5")
+
+
+function emoefe(){
+	slide1.style.overflow = "scroll"
+}
+
+function emoefe1(){
+	slide2.style.overflow = "scroll"
+}
+
+function emoefe2(){
+	slide3.style.overflow = "scroll"
+}
+
+function emoefe3(){
+	slide4.style.overflow = "scroll"
+}
+
+function emoefe4(){
+	slide5.style.overflow = "scroll"
+}
+
+
+
+
+
+button1.addEventListener("click",emoefe)
+
+
+button2.addEventListener("click",emoefe1)
+
+button3.addEventListener("click",emoefe2)
+
+button4.addEventListener("click",emoefe3)
+
+button5.addEventListener("click",emoefe4)
+
+
+
+
+
+
+// const bioButton = document.querySelector(".bioButton");
+// const Button = document.querySelector(".Button")
+// function emoefe(){
+// 	const read = document.querySelector(".read")
+// 	read.style.overflow = "scroll"
+// }
+
 function blessed(){
 const bio = document.querySelector(".bio");
-		bio.style.overflow = "scroll"
+		bio.style.overflow = "scroll";
+
 }
 bioButton.addEventListener("click", blessed)
